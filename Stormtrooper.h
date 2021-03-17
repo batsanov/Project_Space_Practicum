928ef9cfac34f48b39d843af526fbe801a1ef174
batsanov <72399782+batsanov@users.noreply.github.com>
2021-03-18 01:55:47 +0200

    Update Stormtrooper.h

--- Stormtrooper.h
+++ Stormtrooper.h
@@ -1,32 +1,35 @@
 #pragma once
 #include "Planet.h"
 
-enum Rank { A, B, C, D };
+enum trooperRank {
+  Patroltrooper, Swamptrooper, Jumptrooper, Deathtrooper
+};
 
 
 class Stormtrooper {
 private:
 	char* id;
-	Rank rank;
+	trooperRank rank;
 	char* type;
 	Planet planet;
 public:
 
 	Stormtrooper();
-	Stormtrooper(const char*, Rank, const char*, Planet);
+	Stormtrooper(const char*, trooperRank, const char*, Planet);
 	Stormtrooper(Stormtrooper& otherStormtrooper);
 	~Stormtrooper();
 	Stormtrooper& operator=(const Stormtrooper&);
 
 	void print();
-	char* getId();
-	Rank getRank();
-	char* getType();
+	char* getId() const;
+	trooperRank getRank() const;
+	char* getType() const;
 	Planet getPlanet();
 	void setId(const char*);
-	void setRank(Rank);
+	void setRank(trooperRank);
 	void setType(const char*);
 	void setPlanet(Planet);
 
-
+	friend std::istream& operator >> (std::istream&, Stormtrooper&);
+	friend std::ostream& operator << (std::ostream&, const Stormtrooper);
 };
