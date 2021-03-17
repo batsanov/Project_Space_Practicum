0f363cf95213b864ff673d894747c140cdc571d5
batsanov <72399782+batsanov@users.noreply.github.com>
2021-03-18 01:54:52 +0200

    Update Planet.h

--- Planet.h
+++ Planet.h
@@ -1,25 +1,49 @@
 #pragma once
+#include <iostream>
+
+template <class T>
+
+void print(T& someClass)
+{
+	std::cout << someClass;
+}
+
+template <class T>
+void getUserInput(T& someClass)
+{
+	std::cin >> someClass;
+}
+
+enum type { Chthonian_planet, Carbon_planet, Coreless_planet, Desert_planet };
 
 class Planet {
 private:
 	char* name;
 	char* system;
 	char* republic;
+	type typePlanet;
 
 public:
 
 	Planet();
-	Planet(const char*, const char*, const char*);
+	Planet(const char*, const char*, const char*, type);
 	Planet(Planet& otherPlanet);
+	~Planet();
+	Planet& operator = (const Planet& otherPlanet);
 
-	void print();
-	char* getName();
-	char* getSystem();
-	char* getRepublic();
+	char* getName()const;
+	char* getSystem()const;
+	char* getRepublic()const;
+	type getType()const;
+
+
 	void setName(const char*);
 	void setSystem(const char*);
 	void setRepublic(const char*);
-	Planet& operator = (const Planet& otherPlanet);
+	void setType(type);
+	
 
-	~Planet();
+	friend std::ostream& operator<<(std::ostream& , const Planet& );
+	friend std::istream& operator>>(std::istream&, Planet&);
 };
+
